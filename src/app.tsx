3225ef6d89667fc67883a3d821c0238457df27d5
matheusc1 <cardoso.matheusbs@gmail.com>
2024-07-09 20:08:15 -0300

    feat: create modal for adding guests

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,14 @@
-import { LucideArrowRight, LucideCalendar, LucideMapPin, LucideSettings2, LucideUserRoundPlus } from "lucide-react"
-import { useState } from "react"
+import { LucideArrowRight, LucideAtSign, LucideCalendar, LucideMapPin, LucidePlus, LucideSettings2, LucideUserRoundPlus, LucideX } from "lucide-react"
+import { FormEvent, useState } from "react"
 
 export function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
+
+  const [emailsToInvite, setEmailsToInvite] = useState([
+    'cardoso.matheusbs@gmail.com',
+    'cardoso.matheusb@outlook.com'
+  ])
 
   function openGuestsInput() {
     setIsGuestsInputOpen(true)
@@ -12,11 +18,47 @@ export function App() {
     setIsGuestsInputOpen(false)
   }
 
+  function OpenGuestsModal() {
+    setIsGuestsModalOpen(true)
+  }
+
+  function closeGuestsModal() {
+    setIsGuestsModalOpen(false)
+  }
+
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+    const email = data.get('email')?.toString()
+
+    if (!email) {
+      return
+    }
+
+    if (emailsToInvite.includes(email)) {
+      return
+    } 
+
+    setEmailsToInvite([
+      ...emailsToInvite,
+      email
+    ])
+
+    event.currentTarget.reset()
+  }
+
+  function removeEmailFromInvites(email: string) {
+    const newEmailList = emailsToInvite.filter(invited => invited != email)
+
+    setEmailsToInvite(newEmailList)
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
 
-        <div className="flex flex-col items-center space-y-3">
+        <div className="flex flex-col items-center gap-3">
           <img src="/logo.svg" alt="plann.er" />
           <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
         </div>
@@ -66,14 +108,10 @@ export function App() {
 
           {isGuestsInputOpen && (
             <div className="h-16 bg-zinc-900 p-4 rounded-xl flex items-center shadow-shape gap-3">
-              <div className="flex items-center gap-2 flex-1">
+              <button type="button" onClick={OpenGuestsModal} className="flex items-center gap-2 flex-1 text-left">
                 <LucideUserRoundPlus className="size-5 text-zinc-400" />
-                <input
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                  type="text"
-                  placeholder="Quem estará na viagem?"
-                />
-              </div>
+                <span className="text-lg text-zinc-400 flex-1">Quem estará na viagem?</span>
+              </button>
 
               <button className="bg-lime-300 text-lime-900 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                 Confirmar viagem
@@ -88,6 +126,56 @@ export function App() {
           com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
         </p>
       </div>
+
+      {isGuestsModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Selecionar convidados</h2>
+                <button onClick={closeGuestsModal}>
+                  <LucideX className="size-5 text-zinc-400" />
+                </button>
+              </div>
+              
+              <p className="text-sm text-zinc-400">
+                Os convidados irão receber e-mails para confirmar a participação na viagem.
+              </p>
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+              {emailsToInvite.map(email => (
+                <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                  <span className="text-zinc-300">{email}</span>
+                  <button onClick={() => removeEmailFromInvites(email)} type="button">
+                    <LucideX className="size-4 text-zinc-400" />
+                  </button>
+                </div>
+              ))}
+            </div>
+
+            <div className="w-full h-px bg-zinc-800" />
+
+            <form onSubmit={addNewEmailToInvite} className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+              <div className="px-2 flex flex-1 items-center gap-2">
+                <LucideAtSign className="size-5 text-zinc-400" />
+                <input
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  type="email"
+                  name="email"
+                  autoComplete="off"
+                  placeholder="Digite o e-mail do convidado"
+                />
+              </div>
+
+              <button type="submit" className="bg-lime-300 text-lime-900 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                Convidar
+                <LucidePlus className="size-5" />
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
