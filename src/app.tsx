8e75c52614a2d3621a7309b67ef67e8637edb1ba
matheusc1 <cardoso.matheusbs@gmail.com>
2024-07-09 19:26:32 -0300

    feat: feat: add functionality for inputs on home page

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,17 @@
-import { LucideArrowRight, LucideCalendar, LucideMapPin } from "lucide-react"
+import { LucideArrowRight, LucideCalendar, LucideMapPin, LucideSettings2, LucideUserRoundPlus } from "lucide-react"
+import { useState } from "react"
 
 export function App() {
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
+
+  function openGuestsInput() {
+    setIsGuestsInputOpen(true)
+  }
+
+  function closeGuestsInput() {
+    setIsGuestsInputOpen(false)
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
@@ -10,23 +21,66 @@ export function App() {
           <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
         </div>
         
-        <div className="h-16 bg-zinc-900 p-4 rounded-xl flex items-center shadow-shape gap-3">
-          <div className="flex items-center gap-2 flex-1">
-            <LucideMapPin className="size-5 text-zinc-400" />
-            <input className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" type="text" placeholder="Para onde você vai?" />
+        <div className="space-y-4">
+          <div className="h-16 bg-zinc-900 p-4 rounded-xl flex items-center shadow-shape gap-3">
+            <div className="flex items-center gap-2 flex-1">
+              <LucideMapPin className="size-5 text-zinc-400" />
+              <input
+                disabled={isGuestsInputOpen}
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                type="text"
+                placeholder="Para onde você vai?"
+              />
+            </div>
+
+            <div className="flex items-center gap-2">
+              <LucideCalendar className="size-5 text-zinc-400" />
+              <input
+                disabled={isGuestsInputOpen}
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none w-40"
+                type="text"
+                placeholder="Quando?"
+              />
+            </div>
+
+            <div className="w-px h-6 bg-zinc-800" />
+
+            {isGuestsInputOpen ? (
+              <button
+                onClick={closeGuestsInput}
+                className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700"
+              >
+                Alterar local/data
+                <LucideSettings2 className="size-5" />
+              </button>
+            ) : (
+              <button
+                onClick={openGuestsInput}
+                className="bg-lime-300 text-lime-900 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+              >
+                Continuar
+                <LucideArrowRight className="size-5" />
+              </button>
+            )}
           </div>
 
-          <div className="flex items-center gap-2">
-            <LucideCalendar className="size-5 text-zinc-400" />
-            <input className="bg-transparent text-lg placeholder-zinc-400 outline-none w-40" type="text" placeholder="Quando?" />
-          </div>
+          {isGuestsInputOpen && (
+            <div className="h-16 bg-zinc-900 p-4 rounded-xl flex items-center shadow-shape gap-3">
+              <div className="flex items-center gap-2 flex-1">
+                <LucideUserRoundPlus className="size-5 text-zinc-400" />
+                <input
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  type="text"
+                  placeholder="Quem estará na viagem?"
+                />
+              </div>
 
-          <div className="w-px h-6 bg-zinc-800" />
-
-          <button className="bg-lime-300 text-lime-900 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-            Continuar
-            <LucideArrowRight className="size-5" />
-          </button>
+              <button className="bg-lime-300 text-lime-900 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                Confirmar viagem
+                <LucideArrowRight className="size-5" />
+              </button>
+            </div>
+          )}
         </div>
 
         <p className="text-sm text-zinc-500">
