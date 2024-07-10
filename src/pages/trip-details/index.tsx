efc07d9e27d5b780b0d0341c4d5e35936be2c4a8
matheusc1 <cardoso.matheusbs@gmail.com>
2024-07-10 20:23:15 -0300

    feat: create modal for activity registration

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,6 +1,28 @@
-import { LucideCalendar, LucideCircleCheck, LucideCircleDashed, LucideLink2, LucideMapPin, LucidePlus, LucideSettings2, LucideUserCog } from "lucide-react"
+import {
+  LucideCalendar,
+  LucideCircleCheck,
+  LucideCircleDashed,
+  LucideLink2,
+  LucideMapPin,
+  LucidePlus,
+  LucideSettings2,
+  LucideTag,
+  LucideUserCog,
+  LucideX
+} from "lucide-react"
+import { useState } from "react"
 
 export function TripDetailsPage() {
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
+
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true)
+  }
+
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false)
+  }
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -28,7 +50,7 @@ export function TripDetailsPage() {
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button className="bg-lime-300 text-lime-900 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button onClick={openCreateActivityModal} className="bg-lime-300 text-lime-900 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
               <LucidePlus className="size-5" />
               Cadastrar atividade
             </button>
@@ -134,6 +156,55 @@ export function TripDetailsPage() {
           </div>
         </div>
       </main>
+
+      {isCreateActivityModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Confirmar criação de viagem</h2>
+                <button onClick={closeCreateActivityModal}>
+                  <LucideX className="size-5 text-zinc-400" />
+                </button>
+              </div>
+
+              <p className="text-sm text-zinc-400">
+                Todos convidados podem visualizar as atividades.
+              </p>
+            </div>
+
+            <form className="space-y-3">
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <LucideTag className="size-5 text-zinc-400" />
+                <input
+                  className="bg-transparent text-md placeholder-zinc-400 outline-none flex-1"
+                  type="text"
+                  name="title"
+                  autoComplete="off"
+                  placeholder="Qual a atividade?"
+                />
+              </div>
+
+              <div className="flex items-center gap-2">
+                <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                  <LucideCalendar className="size-5 text-zinc-400" />
+                  <input
+                    className="bg-transparent text-md placeholder-zinc-400 outline-none flex-1"
+                    type="datetime-local"
+                    name="occurs_at"
+                    autoComplete="off"
+                    placeholder="Data e horário da atividade"
+                  />
+                </div>
+              </div>
+
+              <button type="submit" className="w-full bg-lime-300 text-lime-900 rounded-lg px-5 h-12 font-medium flex items-center justify-center gap-2 hover:bg-lime-400">
+                Salvar atividade
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
